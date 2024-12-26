5d004d6b99e5150c256dfce089a821bd7418118a
AyushK0204 <ayu.k.210402@gmail.com>
2024-12-26 15:58:13 +0530

    Added job card component

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import { assets, JobCategories, JobLocations } from "../assets/assets";
+import JobCard from "./JobCard";
 
 const JobListing = () => {
-  const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext);
+  const { isSearched, searchFilter, setSearchFilter, jobs } =
+    useContext(AppContext);
 
   return (
     <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
@@ -47,7 +49,7 @@ const JobListing = () => {
 
         {/* {category filter} */}
         <div className="max-lg:hidden">
-          <h4 className="font-medium text-lg py-4">Search by Work Fields</h4>
+          <h4 className="font-medium text-lg py-4">Search by Profile</h4>
           <ul className="space-y-4 text-gray-600 ">
             {JobCategories.map((category, index) => (
               <li className="flex gap-3 items-center" key={index}>
@@ -63,8 +65,10 @@ const JobListing = () => {
           </ul>
         </div>
         {/* {location filter} */}
-        <div className="max-lg:hidden">
-          <h4 className="font-medium text-lg py-4">Search by Locations</h4>
+        <div className="max-lg:hidden ">
+          <h4 className="font-medium text-lg py-4 pt-14">
+            Search by Locations
+          </h4>
           <ul className="space-y-4 text-gray-600 ">
             {JobLocations.map((location, index) => (
               <li className="flex gap-3 items-center" key={index}>
@@ -80,6 +84,20 @@ const JobListing = () => {
           </ul>
         </div>
       </div>
+
+      {/* {job listings} */}
+
+      <section className="w-full lg:w-3/4 text-gray-800 max-lg:px-4">
+        <h3 className="font-medium text-3xl py-2" id="job list">
+          Latest Jobs
+        </h3>
+        <p className="mb-8">Get your desired job from top companies</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4">
+          {jobs.map((job, index) => (
+            <JobCard key={index} job={job} />
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
