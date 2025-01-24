c1cfc1e229870b361db84316e107504b88f4c008
AyushK0204 <ayu.k.210402@gmail.com>
2025-01-24 19:33:28 +0530

    Added Sentry initial setup and user model

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+import "./config/instrument.js";
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
+import * as Sentry from "@sentry/node";
 
 // Initialize express
 const app = express();
@@ -17,9 +19,14 @@ app.use(express.json());
 app.get("/", (req, res, next) => {
   res.send("api working");
 });
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 
 // Port
 const PORT = process.env.PORT || 5000;
+
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
